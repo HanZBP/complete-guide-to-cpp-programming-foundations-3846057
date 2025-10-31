db4069cbd49a24b7a17aba5d419ab705cb256478
HanZBP <hanzhangjohn@gmail.com>
2025-10-31 21:55:59 +0000

    added a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 motifications
+// Exercise 00_03 motifications dangerous
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
