ac9a5a9cf3025f3af84f8d17bce76fb59d036749
HanZBP <hanzhangjohn@gmail.com>
2025-11-04 13:12:26 +0000

    Specially for Bun Baby

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,21 +1,27 @@
-// Complete Guide to C++ Programming Foundations
-// Exercise 00_03 motifications dangerous
-// Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
+//Han_Cplusplus_Exercise
+#include/*Compiler Directive*/<iostream>//Header File
+int/*Return Type*/main/*Function*/(int argc, char* argv[])//Parameter List
 
-#include <iostream>
+{/*Curly Brackets-Delimited Symbol*/    
+    std::cout<<"Hello Hentai,"<<std::endl;
+//Statements with Semicolon
+    std::cout<<std::endl;
 
-int main(){
-    float num_1, num_2, result;
+#include<cstdio>
 
-    std::cout << "Enter number 1: " << std::flush;
-    std::cin >> num_1;
-    std::cout << "Enter number 2: " << std::flush;
-    std::cin >> num_2;
-    
-    result = num_1 + num_2;
-    
-    std::cout << "The result of the addition is " << result << std::endl;
-    
-    std::cout << std::endl << std::endl;
-    return 0;
+    printf/*Function*/("I got a question for you,\n"/*List of Arguments-One String*/);
+
+#include<string>//Getting String from User
+    std::cout<<std::endl;
+    std::string name;//Defined string variable
+    std::cout<<"Who is Han's sexual slave?:"<<std::flush;//prompt for user input before user inputed
+    std::cin/*only for single word*/>>name;//capture the user input
+    std::cout<<std::endl;
+    std::cout<<"Ok! So, "<<name<<" is the happiest and luckiest girl in the world!"<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"Love you,"<<std::endl;
+    std::cout<<"Han"<<std::endl;
+
+std::cout<<std::endl<<std::endl<<std::endl;
+return 0;
 }
