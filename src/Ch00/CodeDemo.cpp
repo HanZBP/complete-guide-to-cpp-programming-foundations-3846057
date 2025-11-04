6be358f8baf72a5ecc5041c2fa87368b9a3b2e8e
HanZBP <hanzhangjohn@gmail.com>
2025-11-04 19:52:27 +0000

    changes

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,17 +1,29 @@
 //Han_Cplusplus_Exercise
 #include/*Compiler Directive*/<iostream>//Header File
+#include<cstdio>
+#include<string>//Getting String from User
+int your_silly_level, your_cuteness_level =50;//Global Variable Declaration and Initialization
 int/*Return Type*/main/*Function*/(int argc, char* argv[])//Parameter List
 
 {/*Curly Brackets-Delimited Symbol*/    
+    bool/*Boolean*/ how_many_people_I_love = true;
+    std::cout<<"how_many_people_I_am_in_love_with?="<<how_many_people_I_love<<"!"<<std::endl;
+    std::cout<<std::endl;
+    your_silly_level = 50;//Assigning Value to Variable
+    std::cout<<"your_silly_level="<<your_silly_level<<"~"<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"your_cuteness_level="<<your_cuteness_level<<"~"<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"my_feeling_of_you="<<your_silly_level+your_cuteness_level<<"!"<<std::endl;
+    std::cout<<std::endl<<std::endl;
+    unsigned int my_love_level_of_you;
+    my_love_level_of_you = your_silly_level - your_cuteness_level - 520;
+    std::cout<<"my_love_level_of_you="<<my_love_level_of_you<<"!!"<<std::endl;
+    std::cout<<std::endl<<std::endl;
     std::cout<<"Hello Hentai,"<<std::endl;
 //Statements with Semicolon
     std::cout<<std::endl;
-
-#include<cstdio>
-
     printf/*Function*/("I got a question for you,\n"/*List of Arguments-One String*/);
-
-#include<string>//Getting String from User
     std::cout<<std::endl;
     std::string name;//Defined string variable
     std::cout<<"Who is Han's sexual slave?:"<<std::flush;//prompt for user input before user inputed
@@ -21,7 +33,6 @@ int/*Return Type*/main/*Function*/(int argc, char* argv[])//Parameter List
     std::cout<<std::endl;
     std::cout<<"Love you,"<<std::endl;
     std::cout<<"Han"<<std::endl;
-
 std::cout<<std::endl<<std::endl<<std::endl;
 return 0;
 }
